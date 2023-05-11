6a4c9f571430d91639892b8a91797686ca2a3ce6
RicardoAffonso0607 <ricardoraffonso@hotmail.com>
2023-05-10 21:10:32 -0300

    Fiz umas classes e arrumei a template

--- Elemento.h
+++ Elemento.h
@@ -1,19 +1,21 @@
 #pragma once
 
+/* Código baseado no vídeo do Felipe Alvez Barboza, monitor de 2020*/
+
 template <class Tipo> class Elemento
 {
 private:
-	Elemento<Tipo*> pProx;
+	Elemento<Tipo>* pProx;
 	Tipo* dados;
 
 public:
 	Elemento();
 	~Elemento();
 
-	void setProx(Elemento<Tipo*> pP) { pProx = pP; }
+	void setProx(Elemento<Tipo>* pP) { pProx = pP; }
 	void setDado(Tipo* dd) { dados = dd; }
 
-	Elemento<Tipo*> getProx() { return pProx; }
+	Elemento<Tipo>* getProx() { return pProx; }
 	Tipo* getDados() { return dados; }
 };
 
---
+++ Entidade.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "Entidade.h"
+
+Entidade::Entidade() : x(0), y(0), body(sf::Vector2f(100.f, 100.f)), pWindow(NULL)
+{
+
+}
+
+Entidade::~Entidade()
+{
+}
