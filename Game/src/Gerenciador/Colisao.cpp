4ba535d839fcbc2d05beed1cec93a14d262f0960
iLukS <lucasyfm@hotmail.com>
2023-05-22 07:45:01 -0300

    Update Colisao.cpp

--- Game/src/Gerenciador/Colisao.cpp
+++ Game/src/Gerenciador/Colisao.cpp
@@ -19,43 +19,18 @@ namespace Gerenciador{
         //*eMov = nullptr;
     }
 
-    void Colisao::colidiu(){
+    void Colisao::colidiu(ent1, ent2){
         //Calculo::Calculadora::CoordF intersect;
         //Calculo::Calculadora::CoordF centerDistance;
-        //int i, j;
-        //for (i = 0; i < entidadeFixa->getSize(); i++) {
-        //    for (j = 0; j < entidadesMoveis->getSize(); j++) {
-        //        ent1 = (*entidadesFixas)[i];
-        //        ent2 = (*entidadesMoveis)[j];
-
         //        centerDistance.x = ent2->getPosition().x - ent1->getPosition().x;
         //        centerDistance.y = ent2->getPosition().y - ent1->getPosition().y;
-
         //        intersect.x = fabs(centerDistance.x) - (ent1->getSize().x / 2.0f + ent2->getSize().x / 2.0f);
         //        intersect.y = fabs(centerDistance.y) - (ent1->getSize().y / 2.0f + ent2->getSize().y / 2.0f);
-
-        //        if (intersect.x < 0.0f && intersect.y < 0.0f) {
-        //            ent2->collide(ent1, intersect);
-        //        }
-        //    }
-        //}
-        //for (i = 0; i < entidadesMoveis->getSize(); i++){
-        //    for (j = i + 1; j < entidadesMoveis->getSize(); j++) {
-        //        ent1 = (*entidadesMoveis)[i];
-        //        ent2 = (*entidadesMoveis)[j];
-
-        //        centerDistance.x = ent2->getPosition().x - ent1->getPosition().x;
-        //        centerDistance.y = ent2->getPosition().y - ent1->getPosition().y;
-
-        //        intersect.x = fabs(centerDistance.x) - (ent1->getSize().x / 2.0f + ent2->getSize().x / 2.0f);
-        //        intersect.y = fabs(centerDistance.y) - (ent1->getSize().y / 2.0f + ent2->getSize().y / 2.0f);
-
         //        if (intersect.x < 0.0f && intersect.y < 0.0f){
         //            ent2->collide(ent1, intersect);
         //            ent1->collide(ent2, intersect);
         //        }
         //    }
         //}
-        //clear();
     }
 }
