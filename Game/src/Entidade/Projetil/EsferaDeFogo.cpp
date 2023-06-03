4ded0bf6072974bbc9d1d9fbe4fddb44a7d56e4f
iLukS <lucasyfm@hotmail.com>
2023-06-03 09:15:38 -0300

    Update EsferaDeFogo.cpp

--- Game/src/Entidade/Projetil/EsferaDeFogo.cpp
+++ Game/src/Entidade/Projetil/EsferaDeFogo.cpp
@@ -2,7 +2,8 @@
 #include "Entidade/Projetil/EsferaDeFogo.h"
 
 namespace Projeteis {
-	EsferaDeFogo::EsferaDeFogo(sf::Vector2f pos, int id) : Projetil(4)
+	EsferaDeFogo::EsferaDeFogo(sf::Vector2f pos, int id) :
+		damage(4)
 		size(sf::Vector2f(20.f, 20.f))
 	{
 		body.setSize(size);
