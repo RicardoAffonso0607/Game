e54245772971b55d27c52561ee3683c3c190ff51
iLukS <lucasyfm@hotmail.com>
2023-06-03 09:15:24 -0300

    Update EsferaDeGelo.cpp

--- Game/src/Entidade/Projetil/EsferaDeGelo.cpp
+++ Game/src/Entidade/Projetil/EsferaDeGelo.cpp
@@ -2,7 +2,8 @@
 #include "Entidade/Projetil/EsferaDeGelo.h"
 
 namespace Projeteis {
-	EsferaDeGelo::EsferaDeGelo(sf::Vector2f pos, int id) : Projetil(7)
+	EsferaDeGelo::EsferaDeGelo(sf::Vector2f pos, int id) :
+		damage(7),
 		size(sf::Vector2f(20.f, 20.f))
 	{
 		body.setSize(size);
