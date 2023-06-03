55798dbd4557316a424f9c6915755f1dd6d55be9
iLukS <lucasyfm@hotmail.com>
2023-06-03 09:15:20 -0300

    Update Flecha.cpp

--- Game/src/Entidade/Projetil/Flecha.cpp
+++ Game/src/Entidade/Projetil/Flecha.cpp
@@ -2,7 +2,8 @@
 #include "Entidade/Projetil/Flecha.h"
 
 namespace Projeteis {
-	Flecha::Flecha(sf::Vector2f pos, int id) : Projetil(10)
+	Flecha::Flecha(sf::Vector2f pos, int id) :
+		damage(10),
 		size(sf::Vector2f(30.f, 10.f))
 	{
 		body.setSize(size);
