12b61409d647e7dff2bed459d3f9238177e565ee
iLukSbr <lucasyfm@hotmail.com>
2023-06-10 11:55:41 -0300

    vector de ids

--- Game/inc/Gerenciador/Salvamento.h
+++ Game/inc/Gerenciador/Salvamento.h
@@ -7,16 +7,18 @@ namespace Gerenciador{
 	class Salvamento{
 	private:
 		static Salvamento* pSalvamento;
-		multimap<unsigned int, tuple<int, sf::Vector2f, sf::Time, int32_t>> dados;
-		multimap<unsigned int, tuple<int, sf::Vector2f, sf::Time, int32_t>>::iterator i;
+		multimap<unsigned int, tuple<int, sf::Vector2f, sf::Time, int>> dados;
+		multimap<unsigned int, tuple<int, sf::Vector2f, sf::Time, int>>::iterator i;
 		fstream progresso;
 
 	public:
 		Salvamento();
 		~Salvamento();
-		void carregar(Grafico* ger_graf);
-		void salvar();
-		void limpar();
+		void carregarID(ListaEntidades* list_ent, unsigned int id);
+		void carregarTodos(ListaEntidades* list_ent);
+		void lerJogoSalvo();
+		void salvarJogo();
+		void limparJogoSalvo();
 		static Salvamento* getSalvamento();
 	};
 }
--- Game/src/Gerenciador/Salvamento.cpp
+++ Game/src/Gerenciador/Salvamento.cpp
@@ -12,10 +12,37 @@ namespace Gerenciador{
 
 	Salvamento::~Salvamento()
 	{
+		dados.clear();
 		pSalvamento = nullptr;
 	}
 
-	void Salvamento::carregarJogo(Grafico* ger_graf)
+	void Salvamento::carregarID(ListaEntidades* list_ent, unsigned int id)
+	{
+		int j;
+		auto range = dados.equal_range(id);
+		for (auto i = range.first; i != range.second; ++i)
+		{
+			for (j = 0; j < list_ent->getSize() && list_ent->getEntity(j)->getId() != id; j++)
+			{
+				j++;
+			}
+			if (j < list_ent->getSize() - 1) {
+				//list_ent->getEntity(i)->setLife(get<0>(i.second));
+				dados.erase(i);
+				j++;
+			}
+			else
+				j = 0;
+		}
+	}
+
+	void Salvamento::carregarTodos(ListaEntidades* list_ent)
+	{
+		vector<unsigned int> id_list{1, 2, 11, 12, 21, 22, 31, 32, 41, 42, 51, 52, 61, 62, 71, 72, 91, 92, 93};
+		for_each(id_list.begin(), id_list.end(), [&](auto& k) {carregarID(list_ent, k);});
+	}
+
+	void Salvamento::lerJogoSalvo()
 	{
 		ifstream progresso("jogo_salvo.csv");
 		if (progresso.is_open()) {
@@ -23,17 +50,27 @@ namespace Gerenciador{
 			int life_tmp;
 			sf::Vector2f pos_tmp;
 			sf::Time clock_tmp;
-			int32_t pontuacao_tmp;
-			string linha;
+			int pontuacao_tmp;
+			string linha, texto;
+			dados.clear();
 			while (getline(progresso, linha))
-			{
-				istringstream linestream(line);
-				getline(linestream, texto, ",");
+			{                                                                                    
+				istringstream linestream(linha);
+				getline(linestream, texto, ',');
 				id_tmp = stoul(texto);
-				getline(linestream, texto, ",");
-
-				dados.insert(id_tmp, make_tuple(life_tmp, pos_tmp, clock_tmp, pontuacao_tmp));
-			progresso.close();
+				getline(linestream, texto, ',');
+				life_tmp = stoi(texto);
+				getline(linestream, texto, ',');
+				pos_tmp.x = stof(texto);
+				getline(linestream, texto, ',');
+				pos_tmp.y = stof(texto);
+				getline(linestream, texto, ',');
+				clock_tmp = sf::milliseconds(stoi(texto));
+				getline(linestream, texto);
+				pontuacao_tmp = stoi(texto);
+				dados.emplace(id_tmp, make_tuple(life_tmp, pos_tmp, clock_tmp, pontuacao_tmp));
+				progresso.close();
+			}
 		}
 		else {
 			string erro = "Não foi possível carregar o progresso do jogo.";
@@ -43,11 +80,6 @@ namespace Gerenciador{
 				exit(1);
 			}
 		}
-	}
-
-	void Salvamento::getDados(ListaEntidades* list_ent)
-	{
-		
 	}
 
 	void Salvamento::salvarJogo()
@@ -60,6 +92,7 @@ namespace Gerenciador{
 					      << get<2>(i.second).asMilliseconds() << "," << get<3>(i.second) << endl;
 			}
 			progresso.close();
+			dados.clear();
 		}
 		else {
 			string erro = "Não foi possível salvar o progresso do jogo.";
@@ -71,11 +104,20 @@ namespace Gerenciador{
 		}
 	}
 
-	void Salvamento::limpar()
+	void Salvamento::limparJogoSalvo()
 	{
 		ofstream progresso("jogo_salvo.txt");
 		progresso.clear();
 		progresso.close();
+		if (!remove("jogo_salvo.txt"))
+		{
+			string erro = "Não foi possível excluir o progresso do jogo.";
+			try { throw runtime_error(erro); }
+			catch (...) {
+				cerr << erro << endl;
+				exit(1);
+			}
+		}
 	}
 
 	Salvamento* Salvamento::getSalvamento()
